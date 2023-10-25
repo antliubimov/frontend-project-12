73b1506a3189719fac0ae4508529cedf2f929b02
Anton Liubimov <ant.liubimov@gmail.com>
2023-10-25 04:31:27 +0000

    feat: Add images

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,6 +1,8 @@
+import imgUrl from '../assets/images/page-not-found-404.jpg';
+
 const PageNotFound = () => (
   <div>
-    <img alt='Страница не найдена' src='/assets/images/page-found-404.jpg'/>
+    <img alt='Страница не найдена' src={imgUrl}/>
     <h1>Страница не найдена</h1>
     <p>но вы можете перейти на главную страницу</p>
   </div>
