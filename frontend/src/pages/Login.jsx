7cd718ddfc6a1c756bdc97f18583d71350ebef80
Anton Liubimov <ant.liubimov@gmail.com>
2023-10-29 18:00:47 +0500

    feat: Add a validation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,25 @@
-import { Formik } from 'formik';
-// import * as Yup from 'yup';
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Image from 'react-bootstrap/Image';
 import loginImg from '../assets/images/login.svg';
 
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Обязательное поле'),
+  password: Yup.string()
+    .required('Пожалуйста, введите пароль'),
+});
+
+/* eslint-disable */
+const handleSubmit = (e, values) => {
+  e.preventDefault();
+  console.log(values);
+};
+
+/* eslint-enable */
 const Login = () => (
   <main className="d-flex justify-content-center align-items-center vh-100">
     <div className="d-flex w-75 justify-content-center align-items-center">
@@ -19,25 +33,30 @@ const Login = () => (
             username: '',
             password: '',
           }}
-          onSubmit={(values) => console.log(values)}
+          validationSchema={SignupSchema}
+          onSubmit={handleSubmit}
         >
-          <Form className="d-flex flex-column gap-3">
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Ваш ник"
-              className="mb-3"
-            >
-              <Form.Control name="username" type="text" autocomplete="username" placeholder="username" required />
-            </FloatingLabel>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Пароль"
-              className="mb-3"
-            >
-              <Form.Control name="password" type="password" placeholder="Пароль" required />
-            </FloatingLabel>
-            <Button variant="outline-primary" type="submit">Войти</Button>
-          </Form>
+          {() => (
+            <Form className="d-flex flex-column gap-3">
+              <FloatingLabel
+                controlId="floatingInputUsername"
+                label="Ваш ник"
+                className="mb-3"
+              >
+                <Form.Control name="username" type="text" placeholder="username" required />
+              </FloatingLabel>
+              <ErrorMessage name="username" />
+              <FloatingLabel
+                controlId="floatingInputPassword"
+                label="Пароль"
+                className="mb-3"
+              >
+                <Form.Control name="password" type="password" placeholder="Пароль" required />
+              </FloatingLabel>
+              <ErrorMessage name="password" />
+              <Button variant="outline-primary" type="submit">Войти</Button>
+            </Form>
+          )}
         </Formik>
       </div>
     </div>
