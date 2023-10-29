368b748907c942b38da18571a69cb6f597ae02c8
Anton Liubimov <ant.liubimov@gmail.com>
2023-10-29 16:15:54 +0500

    fix: Add classes for a 404-page

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
 import imgUrl from '../assets/images/page-not-found-404.jpg';
 
 const PageNotFound = () => (
-  <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-
-    <img className="max-w-sm" alt="Страница не найдена" src={imgUrl} />
-    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Страница не найдена</h1>
-    <p className="mt-6 text-base leading-7 text-gray-600">Извините, мы не смогли найти страницу, которую вы ищете</p>
-    <a href="/" className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm my-6 font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Перейти на главную страницу</a>
+  <main className="container-fluid d-flex flex-column col-8 align-items-center justify-content-center vh-100">
+    <Image className="col-8" alt="Страница не найдена" src={imgUrl} />
+    <h1 className="h1">Страница не найдена</h1>
+    <p className="lead">Извините, мы не смогли найти страницу, которую вы ищете</p>
+    <Link to="/" className="link-underline-primary link-offset-2">Перейти на главную страницу</Link>
   </main>
 );
 
