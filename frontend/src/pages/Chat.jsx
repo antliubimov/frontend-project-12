559d501976dcd77ab0d7ecba974b770eee2d06b7
Anton Liubimov <ant.liubimov@gmail.com>
2023-11-10 22:32:40 +0500

    feat: Check a token in localStorage

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import routes from '../routes/routes';
+import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+import useAuth from '../hooks/index.jsx';
+import routes from '../routes/routes';
 
 const ChatPage = () => {
   const tmp = 'Hello in this chat';
+  const auth = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
-  }, []);
+    const headers = auth.getAuthHeader();
+    if (!headers?.Authorization) {
+      navigate(routes.loginPagePath());
+    }
+  }, [auth, navigate]);
 
   return (
     <div>{tmp}</div>
