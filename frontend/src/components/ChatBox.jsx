f5199b8d79dacb1e8fa748ad0acb47a51cd2d093
Anton Liubimov <ant.liubimov@gmail.com>
2023-11-12 22:51:50 +0500

    feat: add styles of chatbox

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -3,24 +3,32 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
+
+const Channel = () => (
+
+);
 
 const ChatBox = () => (
   <Container className="my-4 h-100 rounded shadow">
-    <Row>
+    <Row className="h-100 bg-white flex-md-row">
       <Col xs={4} md={2} className="bg-light">
         <Row className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-          <b className="p-0">Каналы</b>
-          <button type="button" className="p-0 text-primary btn btn-group-vertical">
+          <b className="p-0 w-auto">Каналы</b>
+          <button type="button" className="p-0 w-auto text-primary btn btn-group-vertical">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>
           </button>
         </Row>
-        <ListGroup>
-          <ListGroup.Item>channel</ListGroup.Item>
-        </ListGroup>
+        <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+          <li className="nav-item w-100">
+            <button type="button" className="w-100 rounded-0 text-start btn btn-secondary">
+              <span className="me-1">#</span>
+              channel
+            </button>
+          </li>
+        </ul>
       </Col>
       <Col xs={8} md={10}>v</Col>
     </Row>
