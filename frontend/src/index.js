160a3e10f3ce8cd3426a2323141eb4d086b349d3
Anton Liubimov <ant.liubimov@gmail.com>
2023-12-24 20:21:31 +0500

    feat: Add socket.io

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { io } from 'socket.io-client';
 import init from './init.jsx';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  const vdom = await init();
+  const socket = io();
+  const vdom = await init(socket);
   root.render(<React.StrictMode>{vdom}</React.StrictMode>);
 };
 
