82798fa0f10f1bd74ab11b0b3fe702eac6ed1e5e
Anton Liubimov <ant.liubimov@gmail.com>
2023-12-24 20:24:41 +0500

    feat: Add socket connection for newMessage

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -6,9 +6,10 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './components/App';
 import resources from './locales/index.js';
 import './assets/styles/index.css';
-import reducer from './slices/index.js';
+import reducer, { actions } from './slices/index.js';
+import { ApiContext } from './contexts/index';
 
-const init = async () => {
+const init = async (socket) => {
   const i18n = i18next.createInstance();
   await i18n
     .use(initReactI18next)
@@ -17,14 +18,45 @@ const init = async () => {
       fallbackLng: 'ru',
     });
 
+  const withAcknowledgement = (socketFunc) => (...args) => new Promise((resolve, reject) => {
+    let state = 'pending'; // eslint-disable-line
+    const timer = setTimeout(() => {
+      state = 'rejected';
+      reject();
+    }, 3000);
+
+    socketFunc(...args, (response) => {
+      if (state !== 'pending') return;
+
+      clearTimeout(timer);
+
+      if (response.status === 'ok') {
+        state = 'resolved';
+        resolve(response.data);
+      }
+
+      reject();
+    });
+  });
+
+  const api = {
+    sendMessage: withAcknowledgement((...args) => socket.volatile.emit('newMessage', ...args)),
+  };
+
   const store = configureStore({
     reducer,
+  });
+
+  socket.on('newMessage', (payload) => {
+    store.dispatch(actions.addMessage({ message: payload }));
   });
 
   return (
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
-        <App />
+        <ApiContext.Provider value={api}>
+          <App />
+        </ApiContext.Provider>
       </I18nextProvider>
     </Provider>
   );
