d00029b8868ee9644c7a33a3cbfb289a3c9606ac
Anton Liubimov <ant.liubimov@gmail.com>
2024-01-10 22:28:51 +0500

    feat: Add modal slice

--- frontend/src/components/ChannelsBox.jsx
+++ frontend/src/components/ChannelsBox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, ButtonGroup, Dropdown } from 'react-bootstrap';
 import { PlusCircleFill } from 'react-bootstrap-icons';
+import { actions } from '../slices';
 
-const Channel = ({ channel, isCurrent }) => {
+const Channel = ({ channel, isCurrent, handleSelect }) => {
   const { t } = useTranslation();
   const variant = isCurrent ? 'secondary' : null;
 
@@ -13,7 +14,12 @@ const Channel = ({ channel, isCurrent }) => {
       {channel.removable
         ? (
           <Dropdown as={ButtonGroup} className="d-flex">
-            <Button key={channel.id} variant={variant} className="w-100 rounded-0 text-light">
+            <Button
+              key={channel.id}
+              variant={variant}
+              className="w-100 rounded-0 text-light"
+              onClick={handleSelect}
+            >
               <span className="me-1">#</span>
               {channel.name}
             </Button>
@@ -31,6 +37,7 @@ const Channel = ({ channel, isCurrent }) => {
             variant={variant}
             type="button"
             className="w-100 rounded-0 text-start text-light"
+            onClick={handleSelect}
           >
             <span className="me-1">#</span>
             {channel.name}
@@ -43,12 +50,24 @@ const Channel = ({ channel, isCurrent }) => {
 const ChannelsBox = () => {
   const { t } = useTranslation();
   const { channels, currentChannelId } = useSelector((state) => state.channelsReducer);
+  const dispatch = useDispatch();
+
+  const handleSelectChannel = (channelId) => () => {
+    dispatch(actions.setCurrentChannel({ channelId }));
+  };
+  const handleAddChannel = () => {
+    dispatch(actions.openModal({ type: 'addChannel' }));
+  };
 
   return (
     <>
       <div className="d-flex mt-1 justify-content-between mb-2 pe-2 p-4 text-light">
         <b className="p-0 m-0 w-auto h3">{t('channels.channels')}</b>
-        <button type="button" className="p-0 w-auto text-primary btn btn-group-vertical">
+        <button
+          type="button"
+          className="p-0 w-auto text-primary btn btn-group-vertical"
+          onClick={handleAddChannel}
+        >
           <PlusCircleFill size={20} color="#ffffff" />
         </button>
       </div>
@@ -58,6 +77,7 @@ const ChannelsBox = () => {
             key={channel.id}
             channel={channel}
             isCurrent={channel.id === currentChannelId}
+            handleSelect={handleSelectChannel(channel.id)}
           />
         ))}
       </ul>
--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import messagesReducer, { actions as messagesActions } from './messagesSlice';
 import channelsReducer, { actions as channelsActions, defaultChannelId } from './channelsSlice';
+import modalReducer, { actions as modalActions } from './modalSlice';
 
 const actions = {
   ...messagesActions,
   ...channelsActions,
+  ...modalActions,
 };
 
 export {
@@ -15,4 +17,5 @@ export {
 export default combineReducers({
   messagesReducer,
   channelsReducer,
+  modalReducer,
 });
