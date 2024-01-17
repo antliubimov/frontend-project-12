f6ac0a98f26669ce40ea41ff1adce616dbe4bfa7
Anton Liubimov <ant.liubimov@gmail.com>
2024-01-17 10:51:21 +0000

    feat: Add validation schema to modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,1 +1,13 @@
 import React from 'react';
+import * as yup from 'yup';
+
+const getValidationSchema = (channels) => yup.object.shape({
+  name: yup
+    .string()
+    .trim()
+    .min(3, 'modal.min')
+    .max(20, 'modal.max')
+    .required('modal.required')
+    .notOneOf(channels, 'modal.uniq')
+});
+
--- frontend/src/locales/en.js
+++ frontend/src/locales/en.js
@@ -16,5 +16,14 @@ export default {
       messageCount_one: 'message',
       messageCount_other: 'messages',
     },
+    modal: {
+      add: 'Add channel',
+      rename: 'Rename channel',
+      remove: 'Remove channel',
+      required: 'Required',
+      min: 'Must be 3 to 20 characters',
+      max: 'Must be 3 to 20 characters',
+      uniq: 'Must be unique',
+    },
   },
 };
